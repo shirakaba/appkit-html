94c5c474a5f6ef75a78d8603d4090885e5d68f10
shirakaba <14055146+shirakaba@users.noreply.github.com>
2023-10-12 21:35:35 +0900

    test setters

--- example/html.js
+++ example/html.js
@@ -1,12 +1,15 @@
 import { polyfill } from '../dist/index.js';
 
 await polyfill(globalThis);
-console.log(document.documentElement.outerHTML);
+// console.log(document.documentElement.outerHTML);
 
-const nsWindow = document.createElement('ns-window');
+// const nsWindow = document.createElement('ns-window');
 const nsView = document.createElement('ns-view');
 
-nsView.setAttribute('background-color', 'abc');
+// nsView.setAttribute('background-color', 'abc');
 nsView.backgroundColor = 'abc';
-nsWindow.appendChild(nsView);
-console.log(nsWindow.outerHTML);
+// console.log('nsView.backgroundColor', nsView.backgroundColor);
+// nsWindow.appendChild(nsView);
+// document.body.appendChild(nsWindow);
+// console.log(nsWindow.outerHTML);
+// console.log(document.documentElement.outerHTML);
